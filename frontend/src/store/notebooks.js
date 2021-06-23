4989ecfc617ac46661eac21752069ace5cfafc46
John Michihara <john.michihara@gmail.com>
2021-06-23 06:21:43 -1000

    Create thunk, thunk creator, and action for editing a notebook

--- frontend/src/store/notebooks.js
+++ frontend/src/store/notebooks.js
@@ -2,6 +2,7 @@ import { csrfFetch } from "./csrf";
 
 const SET_NOTEBOOKS = 'notebooks/SET_NOTEBOOKS';
 const ADD_NOTEBOOK = 'notebooks/ADD_NOTEBOOK';
+const EDIT_NOTEBOOK = 'notebooks/EDIT_NOTEBOOK';
 const DELETE_NOTEBOOK = 'notebooks/DELETE_NOTEBOOK';
 
 const setNotebooks = (notebooks) => ({
@@ -14,18 +15,17 @@ const addNotebook = (notebook) => ({
   notebook
 });
 
+const editNotebook = (notebook) => ({
+  type: EDIT_NOTEBOOK,
+  notebook
+})
+
 const deleteNotebook = (notebook) => ({
   type: DELETE_NOTEBOOK,
   notebook
 })
 
-export const getNotebookToDelete = (notebookId) => async (dispatch) => {
-  const res = await csrfFetch(`/api/notebooks/${notebookId}`, {
-    method: 'DELETE'
-  });
-  const notebook = await res.json();
-  dispatch(deleteNotebook(notebook));
-};
+
 
 export const getNotebooks = (userId) => async (dispatch) => {
   const res = await csrfFetch(`/api/users/${userId}/notebooks`);
@@ -41,7 +41,26 @@ export const createNotebook = (userId) => async (dispatch) => {
   const notebook = await res.json();
   dispatch(addNotebook(notebook));
   return notebook;
-}
+};
+
+export const getNotebookToEdit = (notebookData) => async (dispatch) => {
+  const res = await csrfFetch(`/api/notebooks/${notebookData.id}`, {
+    method: 'PUT',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ notebookData })
+  });
+  const notebook = await res.json();
+  dispatch(editNotebook(notebook));
+  return notebook;
+};
+
+export const getNotebookToDelete = (notebookId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/notebooks/${notebookId}`, {
+    method: 'DELETE'
+  });
+  const notebook = await res.json();
+  dispatch(deleteNotebook(notebook));
+};
 
 const initialState = {};
 
@@ -61,6 +80,15 @@ const notebooksReducer = (state = initialState, action) => {
 
     case ADD_NOTEBOOK:
       allNotebooks = {};
+      allNotebooks[action.notebook.id] = action.notebook;
+
+      return {
+        ...state,
+        ...allNotebooks
+      }
+
+    case EDIT_NOTEBOOK:
+      allNotebooks = { ...state };
       allNotebooks[action.notebook.id] = action.notebook;
 
       return {
