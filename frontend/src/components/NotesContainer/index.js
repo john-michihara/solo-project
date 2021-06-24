f5af3baa7fa87a376fdf353e20d1f310e0f25853
John Michihara <john.michihara@gmail.com>
2021-06-24 12:32:25 -1000

    Fix spacing issues on notes

--- frontend/src/components/NotesContainer/index.js
+++ frontend/src/components/NotesContainer/index.js
@@ -4,11 +4,26 @@ import { useParams } from 'react-router-dom';
 import { getNotes } from '../../store/notes';
 import NoteFormModal from '../NoteFormModal';
 import './Notes.css';
+import dateFormat from 'dateformat';
 
 const NotesContainer = () => {
   const dispatch = useDispatch();
   const notes = useSelector(state => Object.values(state.notes));
   const { notebookId } = useParams();
+
+  notes.sort(function (a, b) {
+    if (a.updatedAt < b.updatedAt) return 1;
+    if (a.updatedAt > b.updatedAt) return -1;
+    return 0;
+  });
+
+  const calcDate = (date) => {
+    if (dateFormat(date, 'DDDD') === dateFormat(date, 'dddd')) {
+      return dateFormat(date, 'mediumDate');
+    } else {
+      return dateFormat(date, 'DDDD');
+    }
+  };
 
   useEffect(() => {
     dispatch(getNotes(notebookId));
@@ -23,8 +38,11 @@ const NotesContainer = () => {
       <div className='notes__container'>
         {notes.map(note => (
           <div key={note.id} className='notes'>
-            <h2 className='notes__title'>{note.title}</h2>
-            <p className='notes__content'>{note.content}</p>
+            <div className='notes__stop-overflow'>
+              <div className='notes__title'>{note.title}</div>
+              <div className='notes__content'>{note.content}</div>
+            </div>
+            <div className='notes__date'>{calcDate(note.updatedAt)}</div>
           </div>
         ))}
       </div>
