c55812da62b4dfc0e205f64e87c748b5158334d9
John Michihara <john.michihara@gmail.com>
2021-06-24 08:18:53 -1000

    Prevent parent component from scrolling when modal is open

--- frontend/src/components/NotebookFormModal/index.js
+++ frontend/src/components/NotebookFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal } from '../../context/Modal';
 import NotebookForm from './NotebookForm';
 import './NotebookForm.css'
@@ -6,9 +6,18 @@ import './NotebookForm.css'
 function NotebookFormModal({ notebook }) {
   const [showModal, setShowModal] = useState(false);
 
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body.style.overflow = showModal ? 'hidden' : 'auto';
+  }, [showModal]);
+
   return (
     <>
-      <button onClick={() => setShowModal(true)} className='notebooks__edit'>
+      <button onClick={openModal} className='notebooks__edit'>
         <i className="fas fa-info-circle" />
       </button>
       {showModal && (
