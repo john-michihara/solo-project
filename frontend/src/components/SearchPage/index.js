eab03be130e6213ed07f8d082e8a134b384f88cc
John Michihara <john.michihara@gmail.com>
2021-09-07 14:59:28 -1000

    Adjust search results algorithm

--- frontend/src/components/SearchPage/index.js
+++ frontend/src/components/SearchPage/index.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { getNotebooks } from '../../store/notebooks';
-import { getAllNotes } from '../../store/notes';
-import SearchBar from './SearchBar';
-import SearchList from './SearchList';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getNotebooks } from "../../store/notebooks";
+import { getAllNotes } from "../../store/notes";
+import SearchBar from "./SearchBar";
+import SearchList from "./SearchList";
 
 const SearchPage = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [notebooksDefault, setNotebooksDefault] = useState([]);
   const [notebooks, setNotebooks] = useState([]);
   const [notesDefault, setNotesDefault] = useState([]);
@@ -16,50 +16,40 @@ const SearchPage = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const user = useSelector(state => state.session.user);
-  if (!user) history.push('/');
+  const user = useSelector((state) => state.session.user);
+  if (!user) history.push("/");
 
   const fetchData = async () => {
     const notebooksData = await dispatch(getNotebooks(user.id));
     const notesData = await dispatch(getAllNotes(user.id));
 
+    setNotebooks(notebooksData);
     setNotebooksDefault(notebooksData);
+    setNotes(notesData);
     setNotesDefault(notesData);
   };
 
   const updateInput = async (input) => {
-    const filteredNotebooks = notebooksDefault.filter(notebook => {
-      return notebook.title.toLowerCase().includes(input.toLowerCase())
+    const filteredNotebooks = notebooksDefault.filter((notebook) => {
+      return notebook.title.toLowerCase().includes(input.toLowerCase());
     });
-    const filteredNotes = notesDefault.filter(note => {
+    const filteredNotes = notesDefault.filter((note) => {
       return note.title.toLowerCase().includes(input.toLowerCase());
     });
 
     setInput(input);
-
-    if (!input) {
-      setNotebooks([]);
-      setNotes([]);
-    } else {
-      setNotebooks(filteredNotebooks);
-      setNotes(filteredNotes);
-    }
+    setNotebooks(filteredNotebooks);
+    setNotes(filteredNotes);
   };
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <>
-      <SearchBar
-        input={input}
-        updateInput={updateInput}
-      />
-      <SearchList
-        notebooks={notebooks}
-        notes={notes}
-      />
+      <SearchBar input={input} updateInput={updateInput} />
+      <SearchList notebooks={notebooks} notes={notes} />
     </>
   );
 };
