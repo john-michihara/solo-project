9236bbe92fa8a53ce4523211863bf5a9f97dbc36
John Michihara <john.michihara@gmail.com>
2021-09-08 11:45:48 -1000

    Add Note component to AllNotesContainer to use Quill

--- frontend/src/components/AllNotesContainer/index.js
+++ frontend/src/components/AllNotesContainer/index.js
@@ -1,36 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router-dom';
-import { getAllNotes, getNoteToDelete } from '../../store/notes';
-import dateFormat from 'dateformat';
-import EditNoteFormModal from '../EditNoteFormModal';
-import '../NotesContainer/Notes.css';
+import { useHistory } from "react-router-dom";
+
+import { getAllNotes } from "../../store/notes";
+import Note from "../NotesContainer/Note";
+import "../NotesContainer/Notes.css";
 
 const AllNotesContainer = () => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const notes = useSelector(state => Object.values(state.notes));
-  const user = useSelector(state => state.session.user);
+  const notes = useSelector((state) => Object.values(state.notes));
+  const user = useSelector((state) => state.session.user);
 
-  if (!user) history.push('/');
+  if (!user) history.push("/");
 
   notes.sort(function (a, b) {
     if (a.updatedAt < b.updatedAt) return 1;
     if (a.updatedAt > b.updatedAt) return -1;
     return 0;
   });
-
-  const calcDate = (date) => {
-    if (dateFormat(date, 'DDDD') === dateFormat(date, 'dddd')) {
-      return dateFormat(date, 'mediumDate');
-    } else {
-      return dateFormat(date, 'DDDD');
-    }
-  };
-
-  const handleClickDelete = async (noteId) => {
-    await dispatch(getNoteToDelete(noteId));
-  };
 
   useEffect(() => {
     if (user) {
@@ -40,26 +28,12 @@ const AllNotesContainer = () => {
 
   return (
     <>
-      <div className='notes__header'>
+      <div className="notes__header">
         <h1 className="notes__header-title">All Notes</h1>
       </div>
-      <div className='notes__container'>
-        {notes.map(note => (
-          <div key={note.id} className='notes' style={{ backgroundColor: note.color }}>
-            <div className='notes__stop-overflow'>
-              <div className='notes__title'>{note.title}</div>
-              <div className='notes__content'>{note.content}</div>
-            </div>
-            <div className='notes__bottom-container'>
-              <div className='notes__date'>{calcDate(note.updatedAt)}</div>
-              <div>
-                <EditNoteFormModal note={note} />
-                <button onClick={() => handleClickDelete(note.id)} className='notes__button'>
-                  <i className="fas fa-trash" />
-                </button>
-              </div>
-            </div>
-          </div>
+      <div className="notes__container">
+        {notes.map((note) => (
+          <Note note={note} />
         ))}
       </div>
     </>
