0143cf851d20653ee323b4c436f5b728ee2e3287
John Michihara <john.michihara@gmail.com>
2021-09-08 15:36:31 -1000

    Refactor Note to open oon click in div

--- frontend/src/components/NotesContainer/Note.js
+++ frontend/src/components/NotesContainer/Note.js
@@ -1,18 +1,29 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import dateFormat from "dateformat";
 import Quill from "quill";
 
+import EditNoteForm from "../EditNoteFormModal/EditNoteForm";
 import { getNoteToDelete } from "../../store/notes";
-import EditNoteFormModal from "../EditNoteFormModal";
+import { Modal } from "../../context/Modal";
 
 const Note = ({ note }) => {
   const dispatch = useDispatch();
   const [quillText, setQuillText] = useState();
+  const [showModal, setShowModal] = useState(false);
 
   const handleClickDelete = () => {
     dispatch(getNoteToDelete(note.id));
   };
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.style.overflow = showModal ? "hidden" : "auto";
+  }, [showModal]);
 
   const calcDate = (date) => {
     if (dateFormat(date, "DDDD") === dateFormat(date, "dddd")) {
@@ -36,26 +47,33 @@ const Note = ({ note }) => {
   );
 
   return (
-    <div
-      key={note.id}
-      className="notes"
-      style={{ backgroundColor: note.color }}
-    >
-      <div className="notes__stop-overflow">
-        <div className="notes__title">{note.title}</div>
-        <div className="notes__content">{quillText}</div>
-        <div ref={wrapperRef} style={{ display: "none" }} />
-      </div>
-      <div className="notes__bottom-container">
-        <div className="notes__date">{calcDate(note.updatedAt)}</div>
-        <div>
-          <EditNoteFormModal note={note} />
-          <button onClick={handleClickDelete} className="notes__button">
-            <i className="fas fa-trash" />
-          </button>
+    <>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <EditNoteForm note={note} setShowModal={setShowModal} />
+        </Modal>
+      )}
+      <div
+        key={note.id}
+        className="notes"
+        style={{ backgroundColor: note.color }}
+        onClick={openModal}
+      >
+        <div className="notes__stop-overflow">
+          <div className="notes__title">{note.title}</div>
+          <div className="notes__content">{quillText}</div>
+          <div ref={wrapperRef} style={{ display: "none" }} />
+        </div>
+        <div className="notes__bottom-container">
+          <div className="notes__date">{calcDate(note.updatedAt)}</div>
+          <div>
+            <button onClick={handleClickDelete} className="notes__button">
+              <i className="fas fa-trash" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
